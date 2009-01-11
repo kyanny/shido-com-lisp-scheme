476e630ab09fbee8ce14fad7eb83c19979dc4349
Kensuke Kaneko <kyanny@gmail.com>
2009-01-11 23:25:44 +0900

    add scheme3-practice1

---
+++ scheme3-practice1.scm
@@ -0,0 +1,17 @@
+;; 1
+(cons "hi" "everybody")
+
+;; 2
+(cons 0 '())
+
+;; 3
+(cons 1 (cons 10 100))
+
+;; 4
+(cons 1 (cons 10 (cons 100 '())))
+
+;; 5
+(cons #\I (cons "saw" (cons 3 (cons "girls" '()))))
+
+;; 6
+(cons "Sum of" (cons (cons 1 (cons 2 (cons 3 (cons 4 '())))) (cons "is" (cons 10 '()))))
