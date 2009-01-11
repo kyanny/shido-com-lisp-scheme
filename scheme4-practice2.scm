e11d85ea23b43cfc92d8413680c387b632aee7b7
Kensuke Kaneko <kyanny@gmail.com>
2009-01-12 00:18:36 +0900

    \e

--- scheme4-practice2.scm
+++ scheme4-practice2.scm
@@ -21,3 +21,28 @@
   (define vy (cons v theta1))
   (define interval (time vy))
   (define distance1 (distance vx)))
+
+;; 
+;; 以下、解答例をコピペ。
+;; 
+
+; definition of pi
+(define pi (* 4 (atan 1.0)))
+
+; degree -> radian
+(define (radian deg)
+  (* deg (/ pi 180.0)))
+
+; free fall time
+(define (ff-time vy)
+  (/ (* 2.0 vy) 9.8))
+
+; horizontal distance 
+(define (dx vx t)
+  (* vx t))
+
+; distance
+(define (distance v ang)
+  (dx
+   (* v (cos (radian ang)))                     ; vx
+   (ff-time (* v (sin (radian ang))))))         ; t
